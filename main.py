74e7c9851f36884e2c7146a3ad3538d3a3347188
Adam Isom <adam.r.isom@gmail.com>
2019-11-05 10:07:07 -0700

    add imports to main

---
+++ main.py
@@ -0,0 +1,9 @@
+import algorithms as algo
+import cli
+import load
+from classes.hash import *
+from classes.package import *
+from classes.time_custom import *
+from classes.truck import *
+from tests.general_tests import *
+

309aaf8c619f986033a1f399a4c0c0823aa75e35
Adam Isom <adam.r.isom@gmail.com>
2019-11-12 23:00:26 -0700

    slightly improve organization of the new route_builder

--- classes/route_builder.py
+++ classes/route_builder.py
@@ -16,16 +16,19 @@ class RouteBuilder:
         - location*
         - distance_from_prev: distance from previous stop
         - packages: list of packages to drop off at this location
-        - projected_arrival: a Time_Custom object
+      ( - projected_arrival: a Time_Custom object )
     A Route is then simply a list of Stops.
+    A 'Stop' is upgraded to a 'StopPlus' in build_stops_for_routes.
 
     * A Location is itself a namedtuple of num, landmark, address.
     '''
     Neighbor = namedtuple(
         'Neighbor', ['location', 'distance_from_prev'])
     Stop = namedtuple(
-        'Stop', ['location', 'distance_from_prev',
-                 'packages', 'projected_arrival'])
+        'Stop', ['location', 'distance_from_prev', 'packages'])
+    StopPlus = namedtuple(
+        'StopPlus', ['location', 'distance_from_prev', 'packages',
+                     'projected_arrival'])
 
     def __init__(self, route_parameters):
         self.available_packages = route_parameters['available_packages'],
@@ -39,69 +42,114 @@ class RouteBuilder:
         self.initial_leave_time = route_parameters['initial_leave_time']
 
         self.package_load = []
-        self.simple_route = []
+        self.route = []
         self.candidate_stops = self.candidate_stops()  # a list
 
-        # use below in comprehension (to get all locs for pkg list)
+        # use below in comprehension (to get ALL locs for pkg list)
         self.pkgs_to_locations = dict()  # [pkgs_to_locations for x in pkgidss]
-
-        self.route = []
         self.route_distance = 0
 
     def candidate_stops(self):
-        '''Uses available_packages.'''
+        '''Create list of stops not yet in route which contain
+        available_packages.
+
+        Uses available_packages.'''
         pass
 
     def find_nearest(self, starting_location):
-        '''Uses distances, candidate_stops.'''
+        '''Return nearest neighbor to starting_location.
+
+        Uses distances, candidate_stops.'''
         pass
 
     def pick_packages_for_deliver_with_constraints(self):
-        '''Called by each of the five main ones.'''
+        '''Augment package_load with any other packages that must be delivered
+        with those currently in package_load.
+
+        If unable to meet deliver-with constraints (because max_load would be
+        exceeded), return tuple of negative one plus list of packages to be
+        removed from package_load. Calling methods will recognize the -1.
+
+        SIDE NOTE: It is okay to adjust for the off-by-one error in the csv.
+        Called by each of the five main ones.
+
+        Uses available_packages.'''
         pass
 
     def pick_packages_requiring_this_truck(self):
-        '''Uses available_packages, max_load, package_load,
-        simple_route, truck_number.'''
+        '''Augment package_load with packages that must go on this truck.
+        Do not exceed a package_load size of max_load.
+
+        Uses available_packages, max_load, package_load,
+        route, truck_number.'''
         pass
 
     def pick_packages_with_deadlines(self):
-        '''Uses available_packages, max_load, package_load,
-        simple_route.'''
+        '''Augment package_load with packages that have deadlines, in order of
+        deadline time (earliest first).
+        Do not exceed a package_load size of max_load.
+
+        I think I'll have it only pick up up to half of all packages that
+        have deadlines, on the assumptions that (1) at least two trucks drive
+        out at 8, (2) they'll have plenty of time, and I want to try this at
+        all because (3) I assume not being so restricted on the first truck's
+        route will lead to better average mileage for truck 1 + truck 2.
+
+        Specifically--I really want to pick up packages on the way. That's
+        where most of my mileage savings will be.
+
+        Uses available_packages, max_load, package_load, route.'''
         pass
 
     def pick_packages_on_the_way(self):
-        '''Uses available_packages, max_load, package_load,
-         simple_route, candidate_stops, pkgs_to_locations.'''
+        '''Augment package_load with packages that are going to the same
+        destinations as packages already in package_load.
+        Do not exceed a package_load size of max_load.
+
+        Uses available_packages, max_load, package_load,
+         route, candidate_stops, pkgs_to_locations.'''
         pass
 
     def add_nearby_neighbors(self):
-        '''Uses available_packages, distances, max_load,
-        package_load, simple_route, candidate_stops.'''
+        '''
+        Do not exceed a package_load size of max_load.
+
+        Uses available_packages, distances, max_load,
+        package_load, route, candidate_stops.'''
         pass
 
     def add_stops_at_end(self):
-        '''Uses available_packages, distances, max_load,
-        package_load, simple_route, candidate_stops.'''
+        '''
+        Do not exceed a package_load size of max_load.
+
+        Uses available_packages, distances, max_load,
+        package_load, route, candidate_stops.'''
         pass
 
     def optimize_route(self):
-        '''Uses distances, simple_route.  Purpose: swap segments.'''
+        '''Repeatedly reorder segments (or subroutes) of route until
+        a shorter overall distance cannot be found.
+
+        Uses distances, route.  Purpose: swap segments.'''
         pass
 
     def get_projected_arrival(self):
-        '''Uses speed_function, simple_route, route! (for previous stop).'''
+        '''Return projected arrival time for a stop on a route.
+
+        Uses speed_function, route, route! (for previous stop).'''
         pass
 
-    def build_stops_for_route(self):
-        '''Uses Locations, speed_function, starting_location,
-        initial_leave_time, package_load, simple_route, pkgs_to_locations.'''
+    def add_projected_arrival_times(self):
+        '''Replace each Stop in route with a StopPlus having projected arrival.
+
+        Uses Locations, speed_function, starting_location, initial_leave_time,
+        package_load, route! (route is used to get a previous_stop).'''
         pass
 
     def build_route(self):
         '''Return a delivery route (list of stops).
         Notes:
-        1. The following functions mutate package_load, simple_route,
+        1. The following functions mutate package_load, route,
         and candidate_stops.
         2. If package_load is size max_load after any one of these calls,
         subsequent calls won't add any more to the load or the route.
@@ -113,6 +161,6 @@ class RouteBuilder:
         self.add_nearby_neighbors()
         self.add_stops_at_end()
         self.optimize_route()
-        self.build_stops_for_route()
+        self.add_projected_arrival_times()
 
         return self.route, self.package_load
